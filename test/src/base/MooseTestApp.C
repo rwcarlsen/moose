8b0f46227f91efd140b814682a7d6a9d5cac598d
Alex Lindsay <alexlindsay239@gmail.com>
2017-02-02 10:43:51 -0600

    Move updateMatPropDependency into MooseObjectWarehouseBase. (#8444)
    - Also make material computing threads (e.g. ComputeResidualThread call the
      updateMatPropDependency methods)
    - Make sure that we don't mark as having material property dependencies simply
      because we call setActiveMaterialProperties in FEProblemBase/Subproblem
    - Move from MooseSharedPointer to std::shared_ptr
    - Closed space between right angle brackets

--- test/src/base/MooseTestApp.C
+++ test/src/base/MooseTestApp.C
@@ -127,6 +127,7 @@
 #include "DataStructIC.h"
 
 // Materials
+#include "ErrorMaterial.h"
 #include "MTMaterial.h"
 #include "TypesMaterial.h"
 #include "StatefulMaterial.h"
@@ -424,6 +425,7 @@ MooseTestApp::registerObjects(Factory & factory)
 
   // Materials
   registerMaterial(MTMaterial);
+  registerMaterial(ErrorMaterial);
   registerMaterial(TypesMaterial);
   registerMaterial(StatefulMaterial);
   registerMaterial(SpatialStatefulMaterial);
