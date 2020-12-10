f089f49ae4863a276a77151ae193b4dd096192ef
Robert Carlsen <rwcarlsen@gmail.com>
2020-12-10 11:05:55 -0700

    another debug echo

--- framework/app.mk
+++ framework/app.mk
@@ -429,6 +429,7 @@ $(bindst): $(app_EXEC)
 	$(call patch_rpath,$@,../lib/moose/.)
 	$(eval libnames := $(foreach lib,$(applibs),$(shell grep "dlname='.*'" $(lib) 2>/dev/null | sed -E "s/dlname='(.*)'/\1/g")))
 	$(eval libpaths := $(foreach lib,$(applibs),$(dir $(lib))$(shell grep "dlname='.*'" $(lib) 2>/dev/null | sed -E "s/dlname='(.*)'/\1/g")))
+	@echo "srcdir $$SRC_DIR"
 	@echo "applibs $(applibs)"
 	@echo 'applibs $(applibs)'
 	@echo "libnames $(libnames)"
